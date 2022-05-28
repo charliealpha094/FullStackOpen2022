e5441de70522ee4a1ae43ff2bb142309fd48721c
charliealpha094 <carlosamaral94@gmail.com>
2022-05-28 14:10:44 +0100

    update exercise 1.6

--- part1/unicafe/src/components/Button.js
+++ part1/unicafe/src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
 
 export default Button;
