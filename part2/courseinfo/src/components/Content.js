6d3c238bd424fb9097e389a591c680fc55a8a002
charliealpha094 <carlosamaral94@gmail.com>
2022-05-31 21:30:20 +0100

    exercise 2.1

---
+++ part2/courseinfo/src/components/Content.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Content = (props) => {
+  let parts = props.parts;
+  console.log(props);
+  return (
+    <div>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+export default Content;
